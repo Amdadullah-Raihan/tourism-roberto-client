3a6320eb7d41be710735236c6d55c835f9e4cb36
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-02-20 23:02:16 +0600

    router set up done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Rooms from './components/Rooms/Rooms';
+import AboutUs from './components/AboutUs/AboutUs';
+import Blogs from './components/Blogs/Blogs';
+import Contact from './components/Contact/Contact';
+import SingleRoom from './components/Rooms/SingleRoom';
+import PrivateRoute from './components/PrivateRoutes/PrivateRoute';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-       
+       <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<AboutUs/>} />
+          <Route path='/rooms' element={<Rooms/>} />
+          <Route path='/single-room' element={<SingleRoom/>} />
+          <Route path='/blogs' element={<Blogs/>} />
+          <Route path='/single-blog' element={<Blogs/>} />
+          <Route path='/blogs' element={<Blogs/>} />
+          <Route path='/contact' element={<Contact/>} />
+
+          {/* private routes starts */}
+
+          <Route path=' ' element={<PrivateRoute></PrivateRoute>} />
+          
+          {/* private routes ends */}
+
+
+          {/* Not found page  */}
+          <Route path='*' element={<NotFound/>} />
+
+       </Routes>
     </div>
   );
 }
