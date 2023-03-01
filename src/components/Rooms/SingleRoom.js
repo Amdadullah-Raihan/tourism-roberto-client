96bc71d801e9575c8e8bfe6f14e03e656a98cec0
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-01 10:41:32 +0600

    room services added in single rooms

--- src/components/Rooms/SingleRoom.js
+++ src/components/Rooms/SingleRoom.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CreateMarkUp from '../../hooks/CreateMarkUp';
+import icon1 from '../../images/icons/icon1.png.webp'
+import icon2 from '../../images/icons/icon2.png.webp'
+import icon3 from '../../images/icons/icon3.png.webp'
+import icon4 from '../../images/icons/icon4.png.webp'
+import icon5 from '../../images/icons/icon5.png.webp'
+import icon6 from '../../images/icons/icon6.png.webp'
 
 const SingleRoom = () => {
 
@@ -21,9 +27,9 @@ const SingleRoom = () => {
     return (
         <div className='md:p-16'>
             <div>
-                <img src={room.imgUrl} alt="" className='w-[70%] mx-auto' />
+                <img src={room.imgUrl} alt="" className='md:w-[70%] ' />
             </div>
-            <div className='w-[60%] grid grid-cols-2 md:grid-cols-4 mx-auto my-16 items-center' >
+            <div className='md:w-[60%] grid grid-cols-2 md:grid-cols-4  md:my-16 items-center' >
                 <div className='border h-32 flex flex-col items-center justify-center px-4 font-bold  text-lg'>
                     <p className='text-gray-500'>
                         Size:
@@ -50,9 +56,33 @@ const SingleRoom = () => {
             </div>
             <div
                 dangerouslySetInnerHTML={CreateMarkUp(room.content)}
-                className='text-start leading-8 w-[70%] text-gray-500'
+                className='text-start leading-8 md:w-[70%] text-gray-500 md:p-0 p-6'
             >
 
+            </div>
+            <div className='text-start px-6'>
+                <h2 className='text-2xl font-bold my-8'>Room Services </h2>
+                <div className="grid grid-cols-2 md:grid-cols-3 md:w-[60%] gap-y-6 ">
+
+                    <div>
+                        <p className='flex gap-2'> <img src={icon1} className='w-12' alt="" /> Air Conditioning</p>
+                    </div>
+                    <div>
+                        <p className='flex gap-2'> <img src={icon2} className='w-10' alt="" /> Free Drinks</p>
+                    </div>
+                    <div>
+                        <p className='flex gap-2'> <img src={icon3} className='w-10' alt="" /> Restuarant Quality</p>
+                    </div>
+                    <div>
+                        <p className='flex gap-2'> <img src={icon4} className='w-10' alt="" /> Cable TV </p>
+                    </div>
+                    <div>
+                        <p className='flex gap-2'> <img src={icon5} className='w-10' alt="" /> Unlimited Wifi</p>
+                    </div>
+                    <div>
+                        <p className='flex gap-2'> <img src={icon6} className='w-10' alt="" /> Services 24/7</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
