e7189f48ef4ffa95c6cc526a1b8991c85f221568
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-01 14:31:03 +0600

    designed 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404!! The page you're looking for was not found!!</h1>
+        <div className='min-h-[50vh] flex flex-col items-center justify-center'>
+            <span className='text-9xl text-red-400 font-bold'>404!!</span>  <br />
+            <span className='text-3xl text-red-400'>Ooops!! The page you're looking for was not found!!</span>
+            <Link to="/">
+                <button className='btn mt-8  bg-teal-500 border-none rounded-none'>Go to home <i className='fa fa-solid fa-arrow-right ml-2'></i></button>
+            </Link>
+
         </div>
     );
 };
