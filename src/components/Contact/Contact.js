f64a1926c48fbbf3acd175be99c088d12aa8bed3
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-02 18:37:46 +0600

    installed and implemented react leaflet

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import useTitle from '../../hooks/useTitle';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 const Contact = () => {
     useTitle("Contact -Roberto ")
     return (
-        <div className='min-h-[73vh]'>
-            
-        </div>
+      <div>
+            <div className='mx-auto'>
+                <MapContainer center={[23.798919, 90.434168]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+      </div>
     );
 };
 
