be9111e49593f8a347c739941dc5e9e602af14c0
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-02 10:34:33 +0600

    table implemented in manageroom

--- src/components/Dashboard/ManageRooms.js
+++ src/components/Dashboard/ManageRooms.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ManageRooms = () => {
+  const [rooms, setRooms] = useState([]);
+
+  //fetching all rooms
+  useEffect(()=>{
+    fetch("http://localhost:5000/rooms")
+    .then(res=> res.json())
+    .then(rooms=> setRooms(rooms))
+  },[])
+
+  //deleting a room
+  const handleDelete = (id) => {
+    fetch("")
+  }
+
+
+
   return (
-    <div>ManageRooms</div>
+    <div>
+
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>title</th>
+              <th>size</th>
+              <th>Capacity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+          
+            { rooms.map((room, i)=>
+              
+            <tr className="hover">
+              <th>{i+1}</th>
+              <td>{room.title}</td>
+              <td>{room.roomSize}</td>
+              <td>{room.capacity}</td>
+              <td role='button' onClick={()=>handleDelete(room._id)} className='text-red-500'>Delete</td>
+            </tr>
+              )}
+          
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
