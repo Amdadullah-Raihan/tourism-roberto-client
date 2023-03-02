a1f7f5f2d656e3ef3e1055744a9730145fb0630b
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-02 19:17:10 +0600

    contact us page design completed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,25 +1,79 @@
 import React from 'react';
 import useTitle from '../../hooks/useTitle';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import img from '../../images/room.avif'
+import { Link } from 'react-router-dom';
+
+
 
 const Contact = () => {
     useTitle("Contact -Roberto ")
     return (
-      <div>
-            <div className='mx-auto'>
-                <MapContainer center={[23.798919, 90.434168]} zoom={13} scrollWheelZoom={false}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[51.505, -0.09]}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
-                </MapContainer>
-            </div>
-      </div>
+        <div className='relative z-0 w-full'>
+            <section className='bg-cover h-[500px] relative' style={{ backgroundImage: `url(${img})` }}>
+                <div className='absolute bg-black h-full w-full opacity-60'></div>
+
+                <div className='relative flex justify-center items-center h-full text-white flex-col leading-10'>
+                    <h1 className='text-4xl'>Contact Us</h1>
+                    <h1 className='text-lg font-semibold'><Link to='/'>Home</Link> <i class="fa-solid fa-chevron-right text-sm"></i> Contact us </h1>
+                </div>
+            </section>
+            <section className='mb-16 shadow-lg bg-white  max-w-[900px] md:mx-auto mt-[-100px] relative z-10 m-4'>
+
+                <div className='grid grid-cols-2 gap-16 mx-auto w-full p-8'>
+                    <div >
+                        <i className='fa fa-solid fa-phone text-teal-500 text-2xl'></i>
+                        <h3 className='md:text-3xl font-semibold'>Phone</h3>
+                        <p className='text-gray-500 font-semibold'>+088-123456789</p>
+                    </div>
+                    <div>
+                        <i className='fa fa-solid fa-location-dot  text-teal-500 text-2xl'></i>
+                        <h3 className='md:text-3xl font-semibold'>Phone</h3>
+                        <p className='text-gray-500 font-semibold'>+088-123456789</p>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-clock  text-teal-500 text-2xl"></i>
+                        <h3 className='md:text-3xl font-semibold'>Phone</h3>
+                        <p className='md:text-gray-500 font-semibold'>+088-123456789</p>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-envelope  text-teal-500 text-2xl" ></i>
+                        <h3 className='md:text-3xl font-semibold'>Phone</h3>
+                        <p className='text-gray-500 font-semibold'>+088-123456789</p>
+                    </div>
+
+                </div>
+                <div className='mx-auto  max-w-[900px] h-auto p-8'>
+                    <MapContainer center={[23.798919, 90.434168]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[51.505, -0.09]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+            </section>
+            <section className='container md:mx-auto p-6 my-16'>
+               <div className='my-8 leading-10'>
+                    <h4 className='text-teal-500 uppercase'>contact us</h4>
+                    <h1 className='font-bold text-4xl capitalize'>leave message</h1>
+               </div>
+                <div>
+                    <div className='flex gap-4 mb-4'>
+                        <input type="text" className='border w-full h-12 pl-4' placeholder='Your Name' />
+                        <input type="text" className='border w-full h-12 pl-4' placeholder='Your Email' />
+                    </div>
+                    <div className=''>
+                        <textarea placeholder='Your Message' name="" id="" className='border w-full h-52 p-4'></textarea >
+                    </div>
+                    <button className="btn my-8 border-none bg-teal-500 rounded-none px-10">Send Message</button>
+                </div>
+            </section>
+        </div>
     );
 };
 
