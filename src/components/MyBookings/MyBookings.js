1787f8614f00d255411b68b1accb82b1a623d7ee
Amdadullah Raihan <amdadullahrayhan@gmail.com>
2023-03-08 22:04:08 +0600

    implemented cancel booking

--- src/components/MyBookings/MyBookings.js
+++ src/components/MyBookings/MyBookings.js
@@ -23,6 +23,26 @@ const MyBookings = () => {
                 setMyBookings(bookings)
             })
     },[])
+
+    //handle booking cancel
+    const handleCancelBooking = (id) => {
+        const url = 'http://localhost:5000/delete-booking'
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ "id": id})
+        })
+            .then(res => res.json())
+            .then(result => {
+               if(result.deletedCount){
+                alert("Canceled your booking")
+                const newBookings = MyBookings.filter(mybooking => mybooking._id !== id )
+                setMyBookings(newBookings)
+               };
+            })
+    }
    
 
   return (
@@ -50,18 +70,37 @@ const MyBookings = () => {
                                 <td>{booking.checkin}</td>
                                 <td>{booking.checkout}</td>
                                 <td>{booking.totalCost}</td>
-                                <td><button className='text-red-500'>Cancel</button></td>
+                                <td><label className='text-red-500 font-semibold' htmlFor='my-modal'>Cancel</label></td>
+                                <input type="checkbox" id="my-modal" className="modal-toggle" />
+                                <div className="modal">
+                                    <div className="modal-box">
+                                        <h3 className="font-bold text-lg">Are you sure?</h3>
+                                        <p className="py-4">Do you want to cancel this booking?</p>
+                                        <div>
+
+                                        </div>
+
+                                        <div className="modal-action">
+                                            <label htmlFor="my-modal" className='bg-red-500 hover:bg-red-400 btn border-none' onClick={() => handleCancelBooking(booking._id)}>Yes!</label>
+                                            <label htmlFor="my-modal" className="btn bg-green-700 border-none">No!</label>
+                                        </div>
+                                    </div>
+                                </div>
                                 
                             </tr>
+                            
                             )
                     }
-                    
-                     
+
+                   
                     
                   </tbody>
                  
                   
               </table>
+
+              
+
           </div>
     </div>
   )
